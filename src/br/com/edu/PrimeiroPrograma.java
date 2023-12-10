b9a3068762c33a74c03641d9dbd57d8b9b2c1ff9
Eduardo Dantas <eduardodanttas@icloud.com>
2023-12-10 16:41:53 -0300

    add first commit

---
+++ src/br/com/edu/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.edu;
+
+
+import br.com.edu.models.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*String nome = "Eduardo";
+        Integer idade = 25;
+        System.out.println("meu nome é: " + nome + " e tenho: " + idade + " anos");*/
+    }
+}
